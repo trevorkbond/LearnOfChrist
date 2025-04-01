13440472239c4ec0f8d2292c0793830a6b17a233
Trevor Bond <trevorkbond@gmail.com>
2025-03-31 22:06:33 -0600

    Further PWA attempts

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,3 +5,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA();
