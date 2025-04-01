c3dac8802e9c46a08d8261bff72f8ea38e94190f
Trevor Bond <trevorkbond@gmail.com>
2025-03-31 21:24:42 -0600

    Add html metadata

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no"
+        />
+      </head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
